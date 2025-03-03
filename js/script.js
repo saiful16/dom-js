bcbf9f4a983847b60dd4c0c7f8386c56fcc9a15b
Saiful <saifulislam.cse164@gmail.com>
2025-03-03 22:32:12 +0100

    quantity and wristsize added

--- js/script.js
+++ js/script.js
@@ -27,3 +27,37 @@ for(let i =0; i < rignButtons.length; i++){
     })
 
 }
+
+function selectWristSize(size){
+    const sizes = ["S", "M", "L","XL"]
+    
+    for(let i = 0; i < sizes.length; i++){
+        const btn = document.getElementById('size-' + sizes[i] )
+        // console.log(btn)
+        if(sizes[i] === size){
+            btn.classList.add('border-purple-400')
+        }
+        else{
+            btn.classList.remove('border-purple-400')
+        }
+    }
+    
+}
+
+const quantityBtn = document.querySelectorAll('.quantity-button')
+for(let btn of quantityBtn){
+    // console.log(btn)
+    btn.addEventListener('click', function(event){
+        const amount = event.target.innerText === "+" ? 1 : -1;
+        // console.log(userInput)
+        const quantityElement = document.getElementById('quantity')
+        const convertedAmount = parseInt(quantityElement.innerText)
+        // console.log(amount, convertedAmount)
+        const newAmount = Math.max(0, amount+convertedAmount)
+        // console.log( typeof newAmount , typeof amount, typeof convertedAmount)
+        console.log(typeof newAmount)
+        quantityElement.innerText = newAmount;
+
+        
+    })
+}
